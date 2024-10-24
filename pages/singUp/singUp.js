052f2dac627a9bccf947bbe8dbcc1a9425debdbb
IgnacioP46 <169081304+IgnacioP46@users.noreply.github.com>
2024-10-24 20:54:38 +0200

    Update singUp.js

--- pages/singUp/singUp.js
+++ pages/singUp/singUp.js
@@ -103,7 +103,7 @@ export function showSignUp() {
                 allowOutsideClick: false,
                 allowEscapeKey: false,
             }).then(() => {
-                window.location.hash = '#home';
+                window.location = 'home';
             });
         }
     });
