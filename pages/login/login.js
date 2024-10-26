f52144b569c8f174de8441832e1d4b8e4237823e
IgnacioP46 <169081304+IgnacioP46@users.noreply.github.com>
2024-10-26 21:03:21 +0200

    Update login.js

--- pages/login/login.js
+++ pages/login/login.js
@@ -61,7 +61,7 @@ export function showLogin() {
                     icon: 'success',
                     confirmButtonText: 'Ir al inicio',
                 }).then(() => {
-                    window.location = '/#homelink';
+                    window.location = '#homelink';
                 });
             } else {
                 Swal.fire({
