43ed090512beb0b28c12cc5f0a0631b408246c96
IgnacioP46 <169081304+IgnacioP46@users.noreply.github.com>
2024-10-26 20:10:34 +0200

    Update footer.js

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,8 +3,8 @@ import "../footer/footer.css";
 export const footer = () => `
 <footer>
     <div class="footer-links">
-        <a class="Instagram" href="https://instagram.com" target="_blank">Instagram</a>
-        <a class="Twitter" href="https://twitter.com" target="_blank">X</a>
+        <a class="Instagram" href="https://www.instagram.com/murmullorecords?igsh=ZjNkdHJxN3U2MTA0" target="_blank">Instagram</a>
+        <a class="Twitter" href="https://x.com/murmullorecords?s=11&t=7InlPLCn4Rq2xL6mg-m1tg" target="_blank">X</a>
     </div>
     <p class = "footerCopy">&copy; 2024 Murmullo Records. Todos los derechos reservados.</p>
     <p class = "footerContact">Contacto: murmullorecords@gmail.com</p>
